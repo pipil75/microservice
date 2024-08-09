ecbbcc988db59fafa6d1856b3c0dcc8b1299a0d6
pipil75 <aprilraphaella75@gmail.com>
2024-08-09 10:03:03 +0200

    projetmicroservice

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -5,9 +5,9 @@ const proxy = require("express-http-proxy");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+const adminUser = require("./middlewear/middlewareGateway");
 app.use("/api/auth", proxy("http://auth:8081"));
-app.use("/api/products", proxy("http://products:8081"));
+app.use("/api/products", adminUser, proxy("http://products:8081"));
 app.listen(PORT, () => {
   console.log(`API Gateway en cours d\'exécution sur le port ${PORT}`);
 });
